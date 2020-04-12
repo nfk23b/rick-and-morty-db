5a453d6b2d2b9c03c11fd0e721539b3d9a8852de
nfk23b <nfk23b@gmail.com>
2020-04-12 18:36:35 +0300

    Updates

---
+++ _next/static/2c4SLaFxs-cciNKZq5P8b/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/rick-and-morty-db/locations/[id]":[a],"/rick-and-morty-db/residents/[id]":[a]}}("static\u002Fchunks\u002F34139558ab4a693746c262fe1f8589d8bf9b6d6f.d2a1012bc15cefe29aee.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
