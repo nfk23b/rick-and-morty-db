5e3ec77cfa17575541bbbd4eeed8753fb94a4091
nfk23b <nfk23b@gmail.com>
2020-04-12 17:28:39 +0300

    Updates

---
+++ _next/static/x0h6NxC28bUnGmg7KRIlz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/locations/[id]":[a],"/residents/[id]":[a]}}("static\u002Fchunks\u002F9bef7ea0c9cbf5de978a559139c22b2d66ab714b.574b930ab99009a12ecd.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
